5b729dd2881113299b911c6b35022f1bfb208fab
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-09-14 12:03:02 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin? ");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("hur långt hoppade Alma? ");
+            string Alma = Console.ReadLine();
+            int iElin = int.Parse(Elin);
+            int iAlma = int.Parse(Alma);
+            int Resultat = iElin - iAlma;
+            Console.WriteLine("Elin hoppade " + Resultat + " meter längre än Alma");
+
+        }
+    }
+}
